275e8102376b424984bb3f5f902d2f97b7c7413a
Gustavo Novaes <gustavo@optionsinformatica.com.br>
2018-08-07 11:30:00 -0300

    'configura-static'

--- core/views.py
+++ core/views.py
@@ -4,8 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    #lista de produtos - extraido do index.html
+    texts = ['loren ipsum','consectetur adipisicing elit', 'sed do eiusmod tempor incididunt',
+             'ut labore et dolore magna aliqua']
+    #dicionadio - cchave e valor
     context = {
-        'title':'Django e-Commerce'
+        'title':'django e-commerce',
+        'texts' : texts
     }
     return render(request,'index.html', context)
 
