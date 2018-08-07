c6cd775ae52478cb3a7cb34ad7158df0e295cf6d
Gustavo Novaes <gustavo@optionsinformatica.com.br>
2018-08-07 17:41:40 -0300

    criando-templates

--- core/views.py
+++ core/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
     #lista de produtos - extraido do index.html
-    texts = ['loren ipsum','consectetur adipisicing elit', 'sed do eiusmod tempor incididunt',
-             'ut labore et dolore magna aliqua']
-    #dicionadio - cchave e valor
-    context = {
-        'title':'django e-commerce',
-        'texts' : texts
-    }
-    return render(request,'index.html', context)
+
+    return render(request,'index.html')
 
